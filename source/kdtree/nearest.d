5cab42d53142c7adc42279cf3b288d3cce41e6ac
Mihail-K <Mihail.K@fimu.tk>
2016-03-25 14:01:50 -0400

    Add nearest neighbour function.

---
+++ source/kdtree/nearest.d
@@ -0,0 +1,53 @@
+
+module kdtree.nearest;
+
+import kdtree.node;
+import kdtree.point;
+
+KDPoint!(k, T) *nearest(size_t k, T)(KDNode!(k, T) *root, KDPoint!(k, T) neighbour)
+{
+    KDPoint!(k, T) *nearest = null;
+    double nearestDistance = double.max;
+
+    void nearestImpl(KDNode!(k, T) *current, KDPoint!(k, T) point, size_t depth = 0)
+    {
+        if(current !is null)
+        {
+            auto axis = depth % k;
+
+            double distance = current.point.distanceSq(point);
+            double distanceAxis = (current.point[axis] - point[axis]) ^^ 2;
+
+            if(nearest is null || distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearest = &current.point;
+            }
+
+            if(nearestDistance > 0)
+            {
+                auto next = distanceAxis > 0 ? current.left : current.right;
+                nearestImpl(next, point, depth + 1);
+
+                if(distanceAxis <= nearestDistance)
+                {
+                    next = distanceAxis > 0 ? current.right : current.left;
+                    nearestImpl(next, point, depth + 1);
+                }
+            }
+        }
+    }
+
+    return nearestImpl(root, neighbour), nearest;
+}
+
+unittest
+{
+    auto root = KDNode!(1, int).build([11], [3], [5], [1]);
+
+    auto result = root.nearest([-24].kdPoint);
+
+    import std.stdio;
+    result.writeln;
+    (*result).writeln;
+}
